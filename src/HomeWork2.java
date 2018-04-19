ab5b493f3db1600968658e76929f6cda5d2aea35
efremov-kv <efremov_kv@mail.ru>
2018-04-19 10:55:19 +0300

    corrected as per issue

--- src/HomeWork2.java
+++ src/HomeWork2.java
@@ -2,27 +2,46 @@ import java.util.Scanner;
 
 public class HomeWork2 {
     public static void main(String args[]) {
-        int a, b;
+        double a, b;
+
         System.out.println("Enter the first integer number:");
-        Scanner scn = new Scanner(System.in);
-        a = scn.nextInt();
-        System.out.println("Enter the second integer number:");
-        b = scn.nextInt();
-        int c = a + b;
-        int d = a - b;
-        int e = a * b;
-        float f = (float) a / b;
+        Scanner mScanner = new Scanner(System.in);
+        a = mScanner.nextInt();
+
+        int x;
         System.out.println("Choose your action by number:");
         System.out.println("1 - sum");
         System.out.println("2 - deduction");
         System.out.println("3 - multiplication");
         System.out.println("4 - division");
-        int x = scn.nextInt();
-        if (x == 1) System.out.println(c);
-        if (x == 2) System.out.println(d);
-        if (x == 3) System.out.println(e);
-        if (x == 4) System.out.println(f);
-        System.out.println("THE END");
 
+        for (x = mScanner.nextInt(); x == 0 || x > 4;) {
+            System.out.println("Choose again!");
+            x = mScanner.nextInt();
+        }
+
+        System.out.println("Enter the second integer number:");
+        for (b = mScanner.nextInt(); b == 0;) {
+            System.out.println("DIVISION BY ZERO !!! Try again:");
+            b = mScanner.nextInt();
+        }
+
+        switch (x) {
+            case 1:
+                System.out.println(a + b);
+                break;
+            case 2:
+                System.out.println(a - b);
+                break;
+            case 3:
+                System.out.println(a * b);
+                break;
+            case 4:
+                System.out.println(a / b);
+                break;
+        }
+        
+        System.out.println("THE END");
     }
 }
+
